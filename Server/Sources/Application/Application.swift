68da5caeb2b3186acdde4836c5bab66701236b63
Frank Lefebvre <dev@franklefebvre.com>
2017-06-20 12:12:07 +0200

    Add log.

--- Server/Sources/Application/Application.swift
+++ Server/Sources/Application/Application.swift
@@ -45,6 +45,7 @@ public func initialize() throws {
         database = couchDBClient.createOrOpenDatabase(name: databaseName, designFile: "../../Resources/main_design.json")
     }
     else if let cloudantService = try? manager.getCloudantService(name: "EasyLogin-Cloudant") {
+        Log.debug("Trying to connect to Cloudant service \(cloudantService)")
         let databaseName = "easy_login"
         let couchDBClient = CouchDBClient(service: cloudantService)
         Log.info("Connected to Cloudant, client = \(couchDBClient), database name = \(databaseName)")
