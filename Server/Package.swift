b3b77157146940798e1a9e0b4d90a4b2d6cc70ad
Frank Lefebvre <dev@franklefebvre.com>
2017-06-20 08:53:27 +0200

    Define CloudConfiguration package minor version.

--- Server/Package.swift
+++ Server/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     	.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 7),
     	.Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 8),
     	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-    	.Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2),
+    	.Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8)        
     ]
 )
