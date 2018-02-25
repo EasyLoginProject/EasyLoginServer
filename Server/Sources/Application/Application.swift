93b8255d1d96245a8039e063ac259f408362c9e0
Yoann Gini <yoann.gini@gmail.com>
2018-02-25 18:02:44 +0100

    Improved logs dring startup

--- Server/Sources/Application/Application.swift
+++ Server/Sources/Application/Application.swift
@@ -39,6 +39,7 @@ public func initialize() throws {
     
     let configurationManager = ConfigProvider.manager
     
+    Log.info("Initializing database access")
     var couchDBClient: CouchDBClient?
     while couchDBClient == nil {
         do {
@@ -49,7 +50,7 @@ public func initialize() throws {
         }
         catch {
             couchDBClient = nil
-            Log.info("CouchDB not available, retrying.")
+            Log.info("CouchDB not available, retrying in 2 seconds.")
             sleep(2)
         }
     }
@@ -60,33 +61,44 @@ public func initialize() throws {
     
     Log.info("Connected to CouchDB, client = \(couchDBClient!), database name = \(databaseName)")
     
+    Log.info("Loading DataProvider")
     let dataProvider = DataProvider(database: database)
+    
+    Log.info("Loading DirectoryService and authenticator middleware")
     let directoryService = EasyLoginDirectoryService(database: database, dataProvider: dataProvider)
     router.all(middleware: EasyLoginAuthenticator(userProvider: database))
     router.all("/db", middleware: directoryService.router())
     
+    Log.info("Loading LDAP router")
     let ldapGatewayAPI = LDAPGatewayAPI(dataProvider: dataProvider)
     router.all("/ldap", middleware: ldapGatewayAPI.router())
     
+    Log.info("Loading Admin API router")
     let adminAPI = AdminAPI(dataProvider: dataProvider)
     router.all("/admapi", middleware: adminAPI.router())
     
+    Log.info("Loading Notifications services")
     let notificationService = installNotificationService()
     inspectorService.registerInspectable(notificationService, name: "notifications")
     
     if let staticSettings = configurationManager["static"] as? [String:Any] {
+        Log.info("Static files requested")
         // --static.admin=/Users/ygi/Sources/EasyLogin/EasyLoginWebAdmin/htdocs/admin
         if let staticAdminPath = staticSettings["admin"] as? String {
+            Log.info("Admin UI as static files")
             router.all("/admin", middleware: StaticFileServer(path: staticAdminPath))
         }
     }
     
+    Log.info("Loading SwiftMetrics")
     // Enable SwiftMetrics Monitoring
     let sm = try SwiftMetrics()
     
     // Pass SwiftMetrics to the dashboard for visualising
     let _ = try SwiftMetricsDash(swiftMetricsInstance: sm, endpoint: router)
     
+    
+    Log.info("Loading Inspector Service")
     inspectorService.installHandlers(to: router)
 }
 
