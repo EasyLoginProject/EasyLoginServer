fb077139e220d5cb2143b8600c329da5654dbad0
Frank Lefebvre <dev@franklefebvre.com>
2018-05-12 23:27:19 +0200

    Remove unused code.

--- Server/Sources/EasyLoginConfiguration/CouchDBClient+EasyLogin.swift
+++ Server/Sources/EasyLoginConfiguration/CouchDBClient+EasyLogin.swift
@@ -2,15 +2,12 @@ import Foundation
 import CouchDB
 import Configuration
 import CloudFoundryConfig
-import SwiftyJSON
 import LoggerAPI
 
 public extension CouchDBClient {
 
     public enum Error: Swift.Error {
         case configurationNotAvailable
-        case databaseNotFound
-        case resourceNotFound
     }
     
     public convenience init(configurationManager: ConfigurationManager) throws {
@@ -55,54 +52,5 @@ public extension CouchDBClient {
         
         Log.debug("Initializing CouchDBClient with manual/default configuration \(connProperties)")
         self.init(connectionProperties: connProperties)
-    }
-    
-    public func createOrOpenDatabase(name: String, designFile: String) -> Database {
-        self.dbExists(name) {
-            exists, error in
-            if !exists {
-                if let error = error {
-                    Log.error("error on dbExists: \(error.localizedDescription)")
-                }
-                self.createDB(name) {
-                    database, error in
-                    guard let database = database else {
-                        let errorMessage = error?.localizedDescription ?? "error == nil"
-                        Log.error("cannot create database: \(errorMessage)")
-                        return
-                    }
-                    guard let json = try? String(contentsOfFile: designFile, encoding:.utf8) else {
-                        Log.error("cannot load file \(designFile)")
-                        return
-                    }
-                    let document = JSON.parse(string: json)
-                    database.createDesign("main_design", document: document) { (result, error) in
-                        Log.info("database index creation: \(String(describing: result))")
-                    }
-                }
-            }
-            else {
-                let database = self.database(name)
-                guard let json = try? String(contentsOfFile: designFile, encoding:.utf8) else {
-                    Log.error("cannot load file \(designFile)")
-                    return
-                }
-                let document = JSON.parse(string: json)
-                self.updateDesignDocument(database: database, document: document)
-            }
-        }
-        return self.database(name)
-    }
-    
-    func updateDesignDocument(database: Database, document: JSON) {
-        database.retrieve("_design/main_design") { (oldDocument, error) in
-            guard let oldDocument = oldDocument else { return }
-            guard let rev = oldDocument["_rev"].string else { return }
-            database.update("_design/main_design", rev: rev, document: document, callback: { (_, _, error) in
-                if error == nil {
-                    Log.info("Design document updated")
-                }
-            })
-        }
     }
 }
